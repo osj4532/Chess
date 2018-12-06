f2baba6cef1ead2a9f9bd824d2589154d3dc6b45
osj4532 <osj4532@daum.net>
2018-12-06 23:45:13 +0900

    chess_version8

---
+++ SelfStudyChess8/src/chess/BlankSpace.java
@@ -0,0 +1,37 @@
+package chess;
+
+import java.util.ArrayList;
+
+public class BlankSpace extends Square{
+
+	public BlankSpace() {
+		super("none", "none");
+		super.setSymbol("   ");
+		setValue(0);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean moveCheck(int[] from, int[] to, String color, boolean check) {
+		return false;
+	}
+
+	@Override
+	public ArrayList<ArrayList<Integer>> attackPathCheck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<ArrayList<Integer>> movePathCheck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<ArrayList<Integer>> checkPathCheck() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
